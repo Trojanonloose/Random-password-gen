098dbe1adf09f5ba17cae897d3d91f8f0b641c15
Trojanonloose <shreyasthakare5@gmail.com>
2021-07-29 15:14:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var password = document.getElementById("password");
+
+function genPassword(){
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+var passwordLength = 12;
+var password = "";
+
+    for(var i = 0; i <= passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
+}
+
+function copyPassword(){
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 999);
+    document.execCommand("copy");
+}
